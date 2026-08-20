318f13b5db6c07c44666b3e3b38dd9b2b37b0d67
Martin <martin.avila@estudiante.uc.cl>
2026-08-20 21:05:55 +0000

    avance ac3

--- Actividades/Actividad 3/main.py
+++ Actividades/Actividad 3/main.py
@@ -5,7 +5,11 @@ if __name__ == "__main__":
     carpeta = input("Ingrese nombre de carpeta: ")
     productos_path = os.path.join(carpeta, "productos.txt")
     acciones_path = os.path.join(carpeta, "acciones.txt")
-
+	inventario=[]
+	rut_lista=[]
+	rut_index=[]
+	index_producto=[]
+	
     # Aqui se leen los productos
     # Por completar: Crear inventario
     with open(productos_path, "r") as productos_file:
@@ -13,7 +17,9 @@ if __name__ == "__main__":
             valores = producto_line.strip().split(",")
             nombre = valores[0]
             precio = int(valores[1])
-            cantidad = int(valores[2])
+            cantidad = int(int(valores[2]))
+            inventario.append([nombre, precio, cantidad])
+            index_producto.append(nombre)
 
     # Aqui se leen las acciones
     # Por completar: Hacer las acciones
@@ -23,4 +29,26 @@ if __name__ == "__main__":
             accion = valores[0]
             rut = valores[1]
             if accion == "Agregar al carro":
-                nombre_producto = valores[2]
+            	producto=valores[2]
+            	if rut not in rut_lista and inventario[index_producto.index(producto)][2]!=0:
+            		rut_lista.append([rut,producto])
+            		rut_index.append(rut)
+            		inventario[index_producto.index(producto)][2]-=1
+            		print(f"[{rut}] {producto} agregado al carro.")
+            	elif inventario[index_producto.index(producto)][2]!=0:
+            		rut_lista[rut_index(rut)].append(producto)
+            		inventario[index_producto.index(producto)][2]-=1
+            		print(f"[{rut}] {producto} agregado al carro.")
+            	else:
+            		print(f"[{rut}] No quedan unidades de {producto}.")
+            elif accion == "Sacar del carro":
+            	if rut not in rut_index:
+            		print("El carro esta vacio, no se ha eliminado ningun elemento del carro.")
+            	elif len(rut_lista[rut_index.index(rut)])==1:
+            		print("El carro esta vacio, no se ha eliminado ningun elemento del carro.")
+            	else:
+            		temporal_producto=rut_lista[rut_index.index(rut)].pop
+            		inventario[index_producto.index(temporal_producto)][2]+=1
+            		print(f"Se ha quitado del carro {temporal_producto}")
+            elif accion == "A":
+				s
